ef860afaa09eac9e06b28c0857e52a9018443b11
Martin Luna <maluna@technisys.com>
2022-01-05 11:53:18 -0300

    Create and import new component list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import JokesList from './components/JokesList'
 
 function App() {
   return (
     <div className="App">
+      <JokesList />
     </div>
   )
 }
---
+++ src/components/JokesList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class JokesList extends Component {
+  render() {
+    return <h1>Jokes List</h1>;
+  }
+}
+
+export default JokesList;

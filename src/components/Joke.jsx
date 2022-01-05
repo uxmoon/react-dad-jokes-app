48575a7ceee1fc77f6b2b7282ab98d59e2c05323
Martin Luna <maluna@technisys.com>
2022-01-05 12:37:39 -0300

    Added buttons to vote joke

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -3,9 +3,16 @@ import React, { Component } from "react";
 class Joke extends Component {
   render() {
     return (
-      <p className="JokesList-item">
-        {this.props.text} - {this.props.votes}
-      </p>
+      <div>
+        <div className="Joke-actions">
+          <p>Votes: {this.props.votes}</p>
+          <button type="button">Upvote</button>
+          <button type="button">Downvote</button>
+        </div>
+        <p className="Joke-text">
+          {this.props.text}
+        </p>
+      </div>
     );
   }
 }

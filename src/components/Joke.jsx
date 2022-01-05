0395963ef88b50da4e821099c3c05c011e39a12c
Martin Luna <maluna@technisys.com>
2022-01-05 14:26:32 -0300

    Added basic styles for Joke comp

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
+import "./Joke.css";
 
 class Joke extends Component {
   render() {
     return (
-      <div>
+      <div className="Joke">
         <div className="Joke-actions">
-          <p>Votes: {this.props.votes}</p>
-          <button type="button" onClick={this.props.upvote}><i class="fas fa-arrow-up"></i>Upvote</button>
-          <button type="button" onClick={this.props.downvote}><i class="fas fa-arrow-down"></i>Downvote</button>
+          <p className="Joke-votes">{this.props.votes}</p>
+          <button type="button" onClick={this.props.upvote}>
+            <i class="fas fa-arrow-up"></i>Upvote
+          </button>
+          <button type="button" onClick={this.props.downvote}>
+            <i class="fas fa-arrow-down"></i>Downvote
+          </button>
         </div>
-        <p className="Joke-text">
-          {this.props.text}
-        </p>
-        <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+        <p className="Joke-text">{this.props.text}</p>
+        <div className="Joke-emoji">
+          <i
+            class="em em-rolling_on_the_floor_laughing"
+            aria-role="presentation"
+            aria-label="ROLLING ON THE FLOOR LAUGHING"
+          ></i>
+        </div>
       </div>
     );
   }

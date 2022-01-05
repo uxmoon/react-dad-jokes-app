bfff825972f98aeef09f1d9f29bfea2873a7f567
Martin Luna <maluna@technisys.com>
2022-01-05 12:10:33 -0300

    Get 10 jokes from API

--- src/components/JokesList.jsx
+++ src/components/JokesList.jsx
@@ -1,6 +1,34 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 class JokesList extends Component {
+  static defaultProps = {
+    jokesNum: 10,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+
+  async componentDidMount() {
+    // create array of jokes
+    let jokes = [];
+
+    // get 10 jokes
+    while (jokes.length < this.props.jokesNum) {
+      // Load jokes
+      let response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      });
+
+      jokes.push(response.data.joke);
+    }
+
+    // console.log(jokes);
+    this.setState({ jokes: jokes });
+  }
+
   render() {
     return <h1>Jokes List</h1>;
   }

f1b2b6b0d1de0f64bfce7dc66d8208fa82acab4a
Martin Luna <maluna@technisys.com>
2022-01-05 14:19:39 -0300

    Added icons and emojis to components

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -6,12 +6,13 @@ class Joke extends Component {
       <div>
         <div className="Joke-actions">
           <p>Votes: {this.props.votes}</p>
-          <button type="button" onClick={this.props.upvote}>Upvote</button>
-          <button type="button" onClick={this.props.downvote}>Downvote</button>
+          <button type="button" onClick={this.props.upvote}><i class="fas fa-arrow-up"></i>Upvote</button>
+          <button type="button" onClick={this.props.downvote}><i class="fas fa-arrow-down"></i>Downvote</button>
         </div>
         <p className="Joke-text">
           {this.props.text}
         </p>
+        <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
       </div>
     );
   }
